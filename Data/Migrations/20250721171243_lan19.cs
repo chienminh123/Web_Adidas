2ef9d4836b42bf7bf18767938e8808787c5717a0
chienminh123 <chien08042k4@gmail.com>
2025-07-22 17:55:30 +0700

    migration

---
+++ Data/Migrations/20250721171243_lan19.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web_Adidas.Data.Migrations
+{
+    public partial class lan19 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MaNguoiDung",
+                table: "DanhGiaPhanHoi",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaNguoiDung",
+                table: "DanhGiaPhanHoi");
+        }
+    }
+}
